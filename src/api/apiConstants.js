b5dbdb80811c61a9941f6fa341ce7ff44dee0b1f
CryptoAngel1024 <cryptoangel1024@gmail.com>
2022-04-14 12:12:00 +0100

    fix

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -1,13 +1,13 @@
 const CONSTANTS = {
   dev: {
-    VITE_API_ENDPOINT: 'https://dev-gladiator.jnj.com/',
-    VITE_S3_BUCKET: 'dev-gladiator-assets',
-    VITE_GA_ENDPOINT: 'https://dev-gladiator.jnj.com/',
+    VITE_API_ENDPOINT: '',
+    VITE_S3_BUCKET: '',
+    VITE_GA_ENDPOINT: '',
   },
   staging: {
-    VITE_API_ENDPOINT: 'https://staging-gladiator.jnj.com/',
-    VITE_S3_BUCKET: 'staging-gladiator-assets',
-    VITE_GA_ENDPOINT: 'https://staging-gladiator.jnj.com/',
+    VITE_API_ENDPOINT: '',
+    VITE_S3_BUCKET: '',
+    VITE_GA_ENDPOINT: '',
   },
 }
 
